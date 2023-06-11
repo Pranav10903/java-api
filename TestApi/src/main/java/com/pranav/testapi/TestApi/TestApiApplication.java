d2fdf9adad8fdc4824a39e6f9840c71549e7b542
Pranav Tatavarthy <97344709+Pranav10903@users.noreply.github.com>
2023-06-11 17:23:38 +0530

    Add files via upload

---
+++ TestApi/src/main/java/com/pranav/testapi/TestApi/TestApiApplication.java
@@ -0,0 +1,14 @@
+package com.pranav.testapi.TestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApiApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		SpringApplication.run(TestApiApplication.class, args);
+	}
+
+}
---
+++ TestApi/src/main/java/com/pranav/testapi/TestApi/topic/TopicService.java
@@ -0,0 +1,41 @@
+package com.pranav.testapi.TestApi.topic;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class TopicService {
+    List<Topic> topics = new ArrayList<>(Arrays.asList(
+            new Topic("Spring", "Spring Framework", "Spring Description"),
+            new Topic("C", "C Framework", "C Description"),
+            new Topic("Java", "Java Framework", "Java Description")
+    ));
+
+    public List<Topic> getAllTopics(){
+        return topics;
+    }
+    public Topic getTopic(String id){
+        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic){
+        topics.add(topic);
+    }
+
+    public void updateTopic(String id, Topic topic) {
+        for(int i = 0; i < topics.size(); i++){
+            Topic t = topics.get(i);
+            if(t.getId().equals(id)){
+                topics.set(i, topic);
+                return;
+            }
+        }
+    }
+
+    public void deleteTopic(String id) {
+        topics.removeIf( t -> t.getId().equals(id));
+    }
+}
